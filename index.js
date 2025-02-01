47f5b7a6c059e51d323918bc9c299cc714ff62ef
MolchanovSergii <molchanse@gmail.com>
2025-02-01 20:40:06 +0200

    add moment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { text } from "./module";
 // import validator from "validator";
 import isEmail from "validator/es/lib/isEmail";
+import moment from "moment";
 
 import "./style.css";
 
 console.log("this text from module " + text);
 // console.log(validator.isEmail("mol6gmail.com"));
 console.log(isEmail("mol@gmail.com"));
+console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
